0f5ddb1f4c1d576ad6f1980802999ab6e38cdee5
Anton <23635473+1progon@users.noreply.github.com>
2022-05-26 18:09:07 +0500

    Add person show placeholder component

---
+++ src/app/view/persons/include/person-show-placeholder/person-show-placeholder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-person-show-placeholder',
+  templateUrl: './person-show-placeholder.component.html',
+  styleUrls: ['./person-show-placeholder.component.scss']
+})
+export class PersonShowPlaceholderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
