61cc678c5029bf5f1587fb60ac0f2d2390fc9358
Anton <23635473+1progon@users.noreply.github.com>
2022-05-31 09:59:39 +0500

    Remove get person by id

--- src/app/view/account/persons/account-person-edit/account-person-edit/account-person-edit.component.ts
+++ src/app/view/account/persons/account-person-edit/account-person-edit/account-person-edit.component.ts
@@ -1,7 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
-import {PersonsService} from "../../../../../services/persons/persons.service";
-import {IPerson} from "../../../../../interfaces/persons/IPerson";
 
 @Component({
   selector: 'app-account-person-edit',
@@ -10,33 +8,17 @@ import {IPerson} from "../../../../../interfaces/persons/IPerson";
 })
 export class AccountPersonEditComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, private personsService: PersonsService) {
+  constructor(private route: ActivatedRoute) {
   }
 
   personId?: number;
-  form: IPerson = <IPerson>{}
 
-  getPerson(id: number) {
-    this.personsService.getPersonEditBase(id)
-      .subscribe({
-        next: value => {
-
-          this.form = value.data;
-
-        }, error: err => {
-          console.error(err)
-        }
-      })
-
-  }
 
   ngOnInit(): void {
     this.personId = this.route.snapshot.params['id']
 
-    //  Get person from backend by id person
-    if (this.personId) {
-      this.getPerson(this.personId);
-    }
+
   }
+
 
 }
