599eb6a9a40ddef67e050b624a50d4e144e1b8a6
Anton <23635473+1progon@users.noreply.github.com>
2022-05-31 09:44:41 +0500

    Remove ref token from guard

--- src/app/guards/only-logged-user-guard.service.ts
+++ src/app/guards/only-logged-user-guard.service.ts
@@ -2,17 +2,13 @@ import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 import {AuthService} from "../services/auth.service";
 import {UserType} from "../enums/UserType";
-import {IResponse} from "../interfaces/IResponse";
-import {TokenDto} from "../dto/user/TokenDto";
-import {environment} from "../../environments/environment";
-import {map, Observable} from "rxjs";
-import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class OnlyLoggedUserGuard implements CanActivate {
-  constructor(private auth: AuthService, private router: Router, private http: HttpClient) {
+  constructor(private auth: AuthService, private router: Router) {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> {
@@ -22,35 +18,8 @@ export class OnlyLoggedUserGuard implements CanActivate {
       return false;
     }
 
-    //Check if token has expired and update it with refresh token first
-    if (this.auth.isLogged() && this.auth.isTokenExpired()) {
-
-      let access_token = this.auth.token?.access_token;
-      let refresh_token = this.auth.token?.refresh_token;
-
-      let body = JSON.stringify(<TokenDto>{access_token, refresh_token});
-
-
-      // ref token
-      return this.http
-        .post<IResponse<TokenDto>>(environment.apiUrl + '/auth/refresh-token', body)
-        .pipe(
-          map(value => {
-
-            this.auth.token = value.data;
-            return this.auth.isLogged() && !this.auth.isTokenExpired();
-
-          })
-        );
-
-
-    }
-
-
     // allow only if user logged and token not expired
-    return this.auth.isLogged() && !this.auth.isTokenExpired();
-
-
+    return this.auth.isLogged();
   }
 
 }
