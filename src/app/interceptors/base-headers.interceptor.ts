341a214726b6d2f53b93f6df0aa86b0d6f006fac
Anton <23635473+1progon@users.noreply.github.com>
2022-05-31 09:45:23 +0500

    Add ref token update

--- src/app/interceptors/base-headers.interceptor.ts
+++ src/app/interceptors/base-headers.interceptor.ts
@@ -1,11 +1,14 @@
 import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {BehaviorSubject, catchError, filter, Observable, switchMap, take, throwError} from 'rxjs';
 import {ActivatedRoute} from "@angular/router";
 import {AuthService} from "../services/auth.service";
+import {TokenDto} from "../dto/user/TokenDto";
+import {IResponse} from "../interfaces/IResponse";
 
 
-export let isRefresh: boolean = false;
+export let isRefreshing: boolean = false;
+export let subj = new BehaviorSubject<null | TokenDto>(null);
 
 
 @Injectable()
@@ -15,19 +18,85 @@ export class BaseHeadersInterceptor implements HttpInterceptor {
               private auth: AuthService) {
   }
 
+  addTokenHeader(request: HttpRequest<any>, token: string): HttpRequest<any> {
+    return request.clone({
+      setHeaders: {
+        "Authorization": `bearer ${token}`
+      }
+    })
+  }
+
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
 
-    let headers: HttpHeaders = new HttpHeaders();
-    headers = headers.set("Content-Type", "application/json");
-
-    if (this.auth.isLogged()) {
-      headers = headers.set("Authorization", "bearer " + this.auth.token?.access_token);
+    if (!(request.body instanceof FormData)) {
+      request = request.clone({
+        setHeaders: {
+          "Content-Type": "application/json",
+        }
+      })
     }
 
-    return next.handle(request.clone({
-      headers
-    }));
+    if (this.auth.isLogged() && this.auth.token) {
+      request = this.addTokenHeader(request, this.auth.token.access_token);
+    }
+
+
+    return next.handle(request)
+      .pipe(catchError(error => {
+        if (error instanceof HttpErrorResponse
+          && error.status == 401 && error.error.data == 'Token is expired') {
+
+          return this.refreshToken(request, next)
+        } else if (error instanceof HttpErrorResponse
+          && error.status == 401 && error.error.data == 'signature is invalid') {
+
+          this.auth.logoutAndRedirect();
+          return throwError(() => error);
+        } else {
+          return throwError(() => error);
+        }
+      }))
+
+
+  }
+
+  refreshToken(request: HttpRequest<any>, next: HttpHandler) {
+
+    if (!isRefreshing) {
+      // start update token
+      isRefreshing = true;
+      subj.next(null);
+
+      // get new token
+      return this.auth.refreshToken()
+        .pipe(switchMap((response: IResponse<TokenDto>) => {
+            // stop refreshing
+            isRefreshing = false;
+
+            // save new token
+            this.auth.token = response.data;
+
+            // add token to subject
+            subj.next(response.data);
+
+            //return request with headers
+            return next.handle(this.addTokenHeader(request, response.data.access_token));
+          })
+        );
+    } else {
+      return subj.pipe(
+        filter(token => token != null),
+        take(1),
+        switchMap(token => {
+          if (token) {
+            return next.handle(this.addTokenHeader(request, token.access_token))
+          }
+          return next.handle(request);
+
+        })
+      )
+    }
 
 
   }
