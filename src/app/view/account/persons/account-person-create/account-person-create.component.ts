c66e13eef69cf6fa9df783158c1d78898aa6bca3
Anton <23635473+1progon@users.noreply.github.com>
2022-05-31 10:05:23 +0500

    Add method on file selected. Implement submit form.

--- src/app/view/account/persons/account-person-create/account-person-create.component.ts
+++ src/app/view/account/persons/account-person-create/account-person-create.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {PersonsService} from "../../../../services/persons/persons.service";
 import {IPerson} from "../../../../interfaces/persons/IPerson";
 
 @Component({
@@ -8,17 +9,82 @@ import {IPerson} from "../../../../interfaces/persons/IPerson";
 })
 export class AccountPersonCreateComponent implements OnInit {
 
-  constructor() {
+  constructor(private personService: PersonsService) {
   }
 
 
-  form: IPerson = <IPerson>{}
+  form = <IPerson>{}
+  imagePreview?: string | ArrayBuffer | null;
+  selectedFile?: File;
+  fileSize?: string;
+  image?: string;
 
   ngOnInit(): void {
   }
 
   submitForm() {
-    console.log(this.form)
+
+    this.form.country.cities = [];
+
+    let fd = new FormData();
+    if (this.selectedFile) {
+      fd.append('image_file', this.selectedFile);
+      fd.append('person_dto', JSON.stringify(this.form));
+    }
+
+    this.personService.addPerson(fd)
+      .subscribe({
+        next: value => {
+          console.log(value)
+
+        }, error: err => console.error(err)
+      })
+
+
   }
 
+  // Convert bytes to KB, MB, GB...
+  niceBytes(n: number) {
+    let units = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    let l = 0;
+
+    while (n >= 1024 && ++l) {
+      n = n / 1024;
+    }
+
+    return (n.toFixed(n < 10 && l > 0 ? 1 : 0) + ' ' + units[l]);
+  }
+
+  // Upload photo and preview in browser
+  onFileSelected(event: Event) {
+    let f = event.target as HTMLInputElement;
+    this.selectedFile = f.files?.[0];
+    if (this.selectedFile) {
+
+      if (!this.selectedFile.type.match('image/(png|jpg|jpeg|gif)')
+        || this.selectedFile.size > 20971520) {
+
+        this.image = undefined;
+        this.selectedFile = undefined;
+        this.fileSize = undefined;
+        this.imagePreview = undefined;
+        return;
+      }
+
+      this.form.image = this.selectedFile.name;
+      this.fileSize = this.niceBytes(this.selectedFile.size);
+
+
+      let reader = new FileReader();
+      reader.readAsDataURL(this.selectedFile);
+
+      // Show image on frontend on upload
+      reader.onload = e => {
+        this.imagePreview = e.target?.result;
+      }
+    }
+
+
+  }
 }
