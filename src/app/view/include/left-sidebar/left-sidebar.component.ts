927e0c08b9e1e93ad2cba79150edfa5435865a1f
Anton <23635473+1progon@users.noreply.github.com>
2022-05-31 09:51:17 +0500

    Add methods for set active country and city

--- src/app/view/include/left-sidebar/left-sidebar.component.ts
+++ src/app/view/include/left-sidebar/left-sidebar.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {LocationService} from "../../../services/locations/location.service";
 import {ICountry} from "../../../interfaces/location/ICountry";
+import {PersonsService} from "../../../services/persons/persons.service";
 
 @Component({
   selector: 'app-left-sidebar',
@@ -12,27 +13,49 @@ import {ICountry} from "../../../interfaces/location/ICountry";
 export class LeftSidebarComponent implements OnInit {
   @Output() closeMenuEvent = new EventEmitter<boolean>();
 
-  constructor(private service: LocationService) {
+  constructor(private service: LocationService,
+              public personsService: PersonsService) {
   }
 
   countries: ICountry[] = [];
   visible: { [id: number]: boolean } = {}
 
   ngOnInit(): void {
+    this.personsService.activeCountrySlug = localStorage.getItem('country') ?? '';
+    this.personsService.activeCitySlug = localStorage.getItem('city') ?? '';
+
     this.service.getCountriesWithPersons()
       .subscribe({
         next: value => {
           this.countries = value.data;
+
+          if (this.personsService.activeCountrySlug != ''
+            && this.personsService.activeCitySlug != '') {
+
+            let country = this.countries
+              .find(c => c.slug == this.personsService.activeCountrySlug);
+            if (!country) {
+              return;
+            }
+
+            this.getCities(country.slug, this.countries.indexOf(country));
+          }
+
         },
         error: err => console.error(err)
       })
+
+
   }
 
-  getCities(countryId: number, elementId: number) {
+  getCities(countrySlug: string, elementId: number) {
     // Collapse - expand cities on country
     this.visible[elementId] = !this.visible[elementId];
 
-    let country = this.countries.find(c => c.id == countryId);
+    let country = this.countries.find(c => c.slug == countrySlug);
+    if (!country) {
+      return;
+    }
 
     //Check if cities already has got
     if (country?.cities) {
@@ -40,7 +63,7 @@ export class LeftSidebarComponent implements OnInit {
     }
 
     // Get cities
-    this.service.getCitiesWithPersonsByCountryId(countryId)
+    this.service.getCitiesWithPersonsByCountryId(country.id)
       .subscribe({
         next: res => {
           if (country) {
@@ -54,5 +77,27 @@ export class LeftSidebarComponent implements OnInit {
 
   closeMenu() {
     this.closeMenuEvent.emit(true)
+  }
+
+  setCountryActive(slug: string) {
+    this.personsService.activeCountrySlug = slug;
+    this.personsService.activeCitySlug = '';
+    localStorage.setItem('country', slug);
+    localStorage.removeItem('city')
+  }
+
+  setCityAndCountryActive(citySlug: string, countrySlug: string) {
+    this.personsService.activeCitySlug = citySlug;
+    this.personsService.activeCountrySlug = countrySlug;
+    localStorage.setItem('country', countrySlug);
+    localStorage.setItem('city', citySlug)
+  }
+
+  resetLocation() {
+    this.personsService.activeCitySlug = '';
+    this.personsService.activeCountrySlug = '';
+    this.visible = {};
+    localStorage.removeItem('country')
+    localStorage.removeItem('city')
   }
 }
