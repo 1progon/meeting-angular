1805b0ea0bc7ce489fb0913caad32aa2b8bef11c
Anton <23635473+1progon@users.noreply.github.com>
2022-05-31 17:11:58 +0500

    Redirect from home to persons if logged

--- src/app/view/homepage/homepage.component.ts
+++ src/app/view/homepage/homepage.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-homepage',
@@ -8,13 +9,17 @@ import {AuthService} from "../../services/auth.service";
 })
 export class HomepageComponent implements OnInit {
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
   }
 
   isLogged = false;
 
   ngOnInit(): void {
     this.isLogged = this.authService.isLogged()
+    if (this.isLogged) {
+      this.router.navigateByUrl('/persons').finally()
+      return
+    }
 
     this.authService.updateTokenEvent
       .subscribe((x) => {
