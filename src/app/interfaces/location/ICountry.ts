9a237c055d8af72ed1bdbb1e611fbf62c47db698
Anton <23635473+1progon@users.noreply.github.com>
2022-06-15 11:04:58 +0500

    Add cities field

--- src/app/interfaces/location/ICountry.ts
+++ src/app/interfaces/location/ICountry.ts
@@ -2,8 +2,8 @@ import {IBaseLocation} from "./IBaseLocation";
 import {IRegion} from "./IRegion";
 import {ICity} from "./ICity";
 
-export interface ICountry extends IBaseLocation{
-  regions: IRegion[];
-  cities: ICity[];
+export interface ICountry extends IBaseLocation {
+  regions?: IRegion[];
+  cities?: ICity[];
   persons_count: number;
 }
--- src/app/interfaces/location/IRegion.ts
+++ src/app/interfaces/location/IRegion.ts
@@ -3,7 +3,7 @@ import {ICity} from "./ICity";
 
 export interface IRegion extends IBaseLocation {
   country_id: number;
-  cities: ICity[];
+  cities?: ICity[];
 }
 
 
